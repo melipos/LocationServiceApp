aa8a0635fafbc281b78228e708db95cfa4a5798d
melipos <tamerium35@gmail.com>
2026-02-11 21:53:09 +0300

    Update LocationService.kt

--- app/src/main/java/com/example/locationservice/LocationService.kt
+++ app/src/main/java/com/example/locationservice/LocationService.kt
@@ -108,6 +108,12 @@ class LocationService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    return START_STICKY
 }
+
+}
+
 
 
