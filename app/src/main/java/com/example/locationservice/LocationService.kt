84c0a2956bb4f859b1c5607704071c2ed8ce97c1
melipos <tamerium35@gmail.com>
2026-02-18 12:51:40 +0300

    Update LocationService.kt

--- app/src/main/java/com/example/locationservice/LocationService.kt
+++ app/src/main/java/com/example/locationservice/LocationService.kt
@@ -14,6 +14,7 @@ import java.io.File
 import java.io.FileOutputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import android.os.PowerManager
 
 class LocationService : Service() {
 
@@ -113,4 +114,5 @@ class LocationService : Service() {
 
     override fun onBind(intent: Intent?): IBinder? = null
 }
+
 
